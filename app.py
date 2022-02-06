421b2b3904200f567a8727cbdce3444a1f62f6fb
AlvaroMAM <alvaroapariciomorales@gmail.com>
2022-02-06 12:10:08 +0100

    nlglsdgl!

--- app.py
+++ app.py
@@ -188,5 +188,5 @@ def profile():
 #-------------User Control----------------------#
         
 if __name__ == '__main__':
-    #app.run(host='127.0.0.1', port=6001, debug=True)
-    app.run()
+    app.run(host='127.0.0.1', port=6001, debug=True)
+    #app.run()
